abeaae98d4a68e4ab854ee504b0f0f89a9e1a27e
Oskari Saarinen <saarinenoskari13@gmail.com>
2022-10-10 14:46:26 +0300

    Stepper is working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
+#include "stepper.hpp"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.write("Hello, world!");
+
+  setupMotor();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int count = 0;
+  while (count < 12) {
+    Motor_CCW(); // make the stepper to anticlockwise rotate
+    count++;
+  }
+
+  count = 0;
+  while (count < 12) {
+    Motor_CW(); //make the stepper to clockwise rotate
+    count++;
+  }
 }
---
+++ src/stepper.hpp
@@ -0,0 +1,7 @@
+#ifndef STEPPER_CPP
+#include <Arduino.h>
+
+void Motor_CCW();
+void Motor_CW();
+void setupMotor();
+#endif
